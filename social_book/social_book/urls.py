c11a0147ab5b0a09c470fd76803ffc7744df24b0
pankajbhaltilak <pankaj.bhaltilak.1@gmail.com>
2022-10-03 16:39:33 +0530

    blogapp

--- social_book/social_book/urls.py
+++ social_book/social_book/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('', include('books.urls'))
+    path('', include('books.urls')),
+    path('', include('blog.urls'))
 ]
