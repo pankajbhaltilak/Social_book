4a285fac11f0d3bf668077af8c89b9775bbdd925
pankajbhaltilak <pankaj.bhaltilak.1@gmail.com>
2022-10-21 09:37:26 +0530

    portfolio

--- social_book/social_book/urls.py
+++ social_book/social_book/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('books.urls')),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('', include('portfolio.urls'))
 ]
